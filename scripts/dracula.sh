e6a16fd86a7c511c981b8033245510e043815819
Ali Anari <ali@anari.io>
2023-08-13 22:12:07 -0700

    Update color scheme

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -29,18 +29,18 @@ main()
   IFS=' ' read -r -a plugins <<< $(get_tmux_option "@dracula-plugins" "battery network weather")
   show_empty_plugins=$(get_tmux_option "@dracula-show-empty-plugins" true)
 
-  # Dracula Color Pallette
-  white='#f8f8f2'
-  gray='#44475a'
-  dark_gray='#282a36'
-  light_purple='#bd93f9'
-  dark_purple='#6272a4'
-  cyan='#8be9fd'
-  green='#50fa7b'
-  orange='#ffb86c'
-  red='#ff5555'
-  pink='#ff79c6'
-  yellow='#f1fa8c'
+  # Yoru Color Palette
+  white='#edeff0'
+  gray='#484a4b'
+  dark_gray='#27292a'
+  light_purple='#c58cec'
+  dark_purple='#bc83e3'
+  cyan='#67afc1'
+  green='#78b892'
+  orange='#E89982'
+  red='#df5b61'
+  pink='#e8646a'
+  yellow='#ecd28b'
 
   # Handle left icon configuration
   case $show_left_icon in
